50653a6294d6b030f81555f6708e06994966b933
iaaankim <iaaankim@gmail.com>
2020-03-04 13:37:41 -0800

    Change content depending on selection

--- src/App.js
+++ src/App.js
@@ -7,25 +7,58 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			mode: 'read',
+			selected_content_id: 1,
 			subject: {title: 'WEB', sub: 'World Wide Web!!'},
+			welcome: {title: 'Welcome', desc: 'Hello, React!!'},
 			content: [
-				{id: 0, title: 'HTML', desc: 'HyperText Markup Language'},
-				{id: 1, title: 'CSS', desc: 'Cascading Style Sheet'},
-				{id: 2, title: 'JavaScript', desc: 'JavaScript is for interaction'},
+				{id: 1, title: 'HTML', desc: 'HyperText Markup Language'},
+				{id: 2, title: 'CSS', desc: 'Cascading Style Sheet'},
+				{id: 3, title: 'JavaScript', desc: 'JavaScript is for interaction'},
 			],
 		}
 	}
 	render() {
+		console.log('App render');
+		let _title, _desc;
+		if (this.state.mode === 'welcome') {
+			_title = this.state.welcome.title;
+			_desc = this.state.welcome.desc;
+		} else if (this.state.mode === 'read') {
+			let i = 0;
+			while(i < this.state.content.length) {
+				let data = this.state.content[i];
+				if(data.id === this.state.selected_content_id) {
+					_title = data.title;
+					_desc = data.desc;
+					break;
+				}
+				i++;
+			}
+		}
 		return (
 			<div className="App">
 				<Subject 
 					title={this.state.subject.title} 
-					sub={this.state.subject.sub}>
+					sub={this.state.subject.sub}
+					onChangePage={function() {
+						this.setState({
+							mode: 'welcome'
+						})
+					}.bind(this)}
+					>
 				</Subject>
 				<TOC
-					data={this.state.content}>
+					data={this.state.content}
+					onChangePage={(id) => {
+						this.setState({
+							mode: 'read',
+							selected_content_id: Number(id)
+						})
+					}}
+					>
 				</TOC>
-				<Content title="HTML" desc='HTML is HyperText Markup Language.'></Content>
+				<Content title={_title} desc={_desc}></Content>
 			</div>
 		);
 	}
