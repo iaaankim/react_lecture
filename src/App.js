c2067da581847f218c48c91f318cc9800cb0e666
iaaankim <iaaankim@gmail.com>
2020-03-04 11:59:17 -0800

    Make toc with state and props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import TOC from './components/Toc';
 import Content from './components/Content';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			subject: {title: 'WEB', sub: 'World Wide Web!!'},
+			content: [
+				{id: 0, title: 'HTML', desc: 'HyperText Markup Language'},
+				{id: 1, title: 'CSS', desc: 'Cascading Style Sheet'},
+				{id: 2, title: 'JavaScript', desc: 'JavaScript is for interaction'},
+			],
+		}
+	}
 	render() {
 		return (
 			<div className="App">
-				<Subject title='WEB' sub='World Wide Web!!'></Subject>
-				<TOC></TOC>
+				<Subject 
+					title={this.state.subject.title} 
+					sub={this.state.subject.sub}>
+				</Subject>
+				<TOC
+					data={this.state.content}>
+				</TOC>
 				<Content title="HTML" desc='HTML is HyperText Markup Language.'></Content>
 			</div>
 		);
