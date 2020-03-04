e75bd52e567ba19a071efbbbea40543cde751a9c
iaaankim <iaaankim@gmail.com>
2020-03-04 11:36:19 -0800

    Divide components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
 import React, { Component } from 'react';
-
-class Subject extends Component {
-	render() {
-		return(
-			<header>
-				<h1>WEB</h1>
-				World Wide Web!
-			</header>
-		);
-	}
-}
-
-class TOC extends Component {
-	render() {
-		return(
-			<nav>
-				<ul>
-					<li><a href='1.html'>HTML</a></li>
-					<li><a href='2.html'>CSS</a></li>
-					<li><a href='3.html'>JavaScript</a></li>
-				</ul>
-			</nav>
-		);
-	}
-}
-
-class Content extends Component {
-	render(){
-		return(
-			<article>
-				<h2>HTML</h2>
-				HTML is HyperText Markup Language.
-			</article>
-		);
-	}
-}
+import Subject from './components/Subject';
+import TOC from './components/Toc';
+import Content from './components/Content';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Subject></Subject>
+				<Subject title='WEB' sub='World Wide Web!!'></Subject>
 				<TOC></TOC>
-				<Content></Content>
+				<Content title="HTML" desc='HTML is HyperText Markup Language.'></Content>
 			</div>
 		);
 	}
